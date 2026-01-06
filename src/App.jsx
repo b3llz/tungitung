d63326f1f801bc58d07c340fe45a6b6abc7a5241
b3llz <160164765+b3llz@users.noreply.github.com>
2026-01-07 02:44:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import CalculatorTab from './CalculatorTab';
+import CalculatorTab from './calculatortab';
 
 function App() {
   return (
-    <div>
+    <div className="bg-slate-50 dark:bg-slate-900 min-h-screen p-6 md:p-12">
       <CalculatorTab />
     </div>
   );
