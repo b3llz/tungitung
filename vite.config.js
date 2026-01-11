08556846b74b53ee49a3de752c4c5956e02321bb
B3LLZ <160164765+b3llz@users.noreply.github.com>
2026-01-11 21:14:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-plugins: [react()],
-base: '/tunggitung/'
+  plugins: [react()],
+  // GANTI 'nama-repo-kamu' dengan nama repository GitHub-mu yang asli
+  // Contoh: jika repo kamu https://github.com/User/kasir-app, maka isinya '/kasir-app/'
+  base: "/tungitung/", 
 })
+
