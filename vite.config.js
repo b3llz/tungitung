2aa75fe30a48288156f20c05e425824df2b4eb6e
B3LLZ <160164765+b3llz@users.noreply.github.com>
2026-01-24 22:09:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GANTI 'nama-repo-kamu' dengan nama repository GitHub-mu yang asli
-  // Contoh: jika repo kamu https://github.com/User/kasir-app, maka isinya '/kasir-app/'
-  base: "/tungitung/", 
+  
+  // HAPUS atau KOMENTARI baris base ini jika deploy ke Vercel!
+  // base: "/tungitung/", 
+  
+  // Tambahkan ini biar Vercel tidak protes soal ukuran file
+  build: {
+    chunkSizeWarningLimit: 1600, 
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    if (id.includes('firebase')) return 'firebase';
+                    if (id.includes('xlsx')) return 'xlsx';
+                    if (id.includes('lucide-react')) return 'lucide';
+                    return 'vendor';
+                }
+            }
+        }
+    }
+  }
 })
-
