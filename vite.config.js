4648da089cfeba344d9a5ea947025e5559c3e960
B3LLZ <160164765+b3llz@users.noreply.github.com>
2026-02-23 13:01:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  
-  // HAPUS atau KOMENTARI baris base ini jika deploy ke Vercel!
-  // base: "/tungitung/", 
-  
-  // Tambahkan ini biar Vercel tidak protes soal ukuran file
+
   build: {
-    chunkSizeWarningLimit: 1600, 
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
-        output: {
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    if (id.includes('firebase')) return 'firebase';
-                    if (id.includes('xlsx')) return 'xlsx';
-                    if (id.includes('lucide-react')) return 'lucide';
-                    return 'vendor';
-                }
-            }
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'firebase'
+            if (id.includes('xlsx')) return 'xlsx'
+            if (id.includes('lucide-react')) return 'lucide'
+            return 'vendor'
+          }
         }
+      }
     }
   }
 })
